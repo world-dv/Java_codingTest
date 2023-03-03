e2f83f4daea5607ae7aec1db2cc3779dabd886db
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-03-03 19:55:10 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ 24264.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+public class Main {
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.print((n * n) + "\n" + 2);
+    }
+}
