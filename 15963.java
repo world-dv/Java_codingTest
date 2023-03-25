55c90baebc625f96a942af54024210e7012790c2
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-03-25 17:49:08 +0900

    CASIO

---
+++ 15963.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        br.close();
+        System.out.println(n == m ? "1" : "0");
+    }
+}
