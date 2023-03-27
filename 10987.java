ec0c2de8085e4d677e352a7bc6d2addf8cf768cc
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-03-27 13:56:57 +0900

    모음의 개수

---
+++ 10987.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
