c98b4850f6f634f68813d77b30d60720a59694ce
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-04-01 22:47:25 +0900

    특별한 날

---
+++ 10768.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if(month < 2) {
+            System.out.println("Before");
+        } else if(month == 2 && day < 18) {
+            System.out.println("Before");
+        } else if (month == 2 && day == 18) {
+            System.out.println("Special");
+        } else if (month == 2 && day > 18) {
+            System.out.println("After");
+        } else if(month > 2) {
+            System.out.println("After");
+        }
+    }
+}
